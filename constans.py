6cb1faa2350eae04c1a01ba1286be6c44842356e
Juan Munoz <juano.mube@gmail.com>
2021-11-28 21:41:06 -0500

    extarnal constants, titles updated

---
+++ constans.py
@@ -0,0 +1,6 @@
+HOME_URL='https://www.larepublica.co/'
+XPATH_LINK_TO_ARTICLE='//text-fill[not(@class)]/a/@href'
+XPATH_TITLE='//h2/span/text()'
+XPATH_SUMMARY='//div[@class="lead"]/p/text()'
+XPATH_BODY='//div[@class="html-content"]//p[not(@class)]/text()'
+COUNT_TEST=1
--- scraper.py
+++ scraper.py
@@ -1,31 +1,34 @@
-from re import L
+from re import L, T
 import requests
 import lxml.html as html
 import os
 import datetime
+import constans
 
+def getCount():
+    return constans.COUNT_TEST
 
+def updateCount():
+    constans.COUNT_TEST=constans.COUNT_TEST +1
 
-HOME_URL='https://www.larepublica.co/'
-XPATH_LINK_TO_ARTICLE='//text-fill[not(@class)]/a/@href'
-XPATH_TITLE='//h2/span/text()'
-XPATH_SUMMARY='//div[@class="lead"]/p/text()'
-XPATH_BODY='//div[@class="html-content"]//p[not(@class)]/text()'
+def get_html_from_response(response, decode_format):
+    notice=response.content.decode(decode_format)                           #decode the site content to utf-8
+    return html.fromstring(notice)                                          #parse de content to html
 
-
-def parse_notice(link, today):
+def parse_news(link, today):
     try:
         response=requests.get(link)
         if response.status_code==200:
-            notice=response.content.decode('utf-8')
-            parsed = html.fromstring(notice)
             
+            parsed = get_html_from_response(response,'utf-8')
             try:
 
-                title=parsed.xpath(XPATH_TITLE)[0]
+                title=parsed.xpath(constans.XPATH_TITLE)[0]
+                title="%s %s"%(str(getCount()), title.replace("#","").replace("|",""))
+                updateCount()
                 print(title)
-                summary=parsed.xpath(XPATH_SUMMARY)[0]
-                body=parsed.xpath(XPATH_BODY)
+                summary=parsed.xpath(constans.XPATH_SUMMARY)[0]
+                body=parsed.xpath(constans.XPATH_BODY)
             except IndexError:
                 return
 
@@ -42,30 +45,31 @@ def parse_notice(link, today):
     except ValueError as ve:
         print(ve)
 
-
-def parse_home():
+def scrap_news():
     try:
-        response=requests.get(HOME_URL)
-        if response.status_code==200:
-            home = response.content.decode('utf-8')
-            parsed = html.fromstring(home)
-            links_to_notices= parsed.xpath(XPATH_LINK_TO_ARTICLE)
-            #print(links_to_notices)
+        response=requests.get(constans.HOME_URL)                            #get the home page
+        if response.status_code==200:                                       #verify that the site is available
+            parsed = get_html_from_response(response,'utf-8')
+            links_to_news= parsed.xpath(constans.XPATH_LINK_TO_ARTICLE)     #get the links of the news
+            today = datetime.date.today().strftime('%d-%m-%Y')              #create the file name using the current date
+            today=f'scraps//{today}'
 
-            today = datetime.date.today().strftime('%d-%m-%Y')
-            if not os.path.isdir(today):
+            if not os.path.isdir(today):                                    #creates the file if it doesnt exist
                 os.mkdir(today)
 
-            for link in links_to_notices:
-                parse_notice(link, today)
+            for link in links_to_news:                                      #scrap each one of the news
+                parse_news(link, today)
 
         else:
             raise ValueError(f'Error: {response.status_code}')
     except ValueError as ve:
         print(ve)
         
+
+
+
 def run():
-    parse_home()
+    scrap_news()
 
 if __name__ == '__main__':
     run()
